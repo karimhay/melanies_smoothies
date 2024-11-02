494a8e12851aa36ac2630b851e2aaeb61be9877e
karimhay <karimhay@gmail.com>
2024-11-02 14:09:29 -0500

    Update streamlit_app.py
    Added the call to the fruitvice API for every ingredient (probably won't work since the fruitvice page is down)

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+        import requests
+        frutivice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -44,8 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, ' +name_on_order+ '!', icon="✅")
-
-# New section to display fruitvice nutrition information
-import requests
-frutivice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(frutivice_response)
